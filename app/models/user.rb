1833fc95607f7f20f37f78fb2a434904b47ab035
Larry Adames <adames.larry@gmail.com>
2015-03-12 00:01:57 -0400

    added tests for post and user model, and for post controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,17 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
   has_many :votes
-  
+
   has_secure_password validations: false
 
   validates_presence_of :username
-  validates_presence_of :password, on: :create, length: {minimum: 5} #minimum is not working.
+  validates :password, presence: true, on: :create, length: {minimum: 5} #minimum is not working.
   validates_uniqueness_of :username
 
   sluggable_column :username
 
   def admin?
-    self.role == 'admin'  
+    self.role == 'admin'
   end
 
   def moderator?
